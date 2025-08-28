44df285b16ffbd87d77c2b17e8df31e00c7f9177
klopez-2024401 <klopez-2024401@kinal.edu.gt>
2025-08-28 14:44:40 -0600

    se agrego nuesvoz paquetes y directorios para html

---
+++ src/main/java/org/kaven/kinvana/KinvanaApplicationWeb.java
@@ -0,0 +1,13 @@
+package org.kaven.kinvana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinvanaApplicationWeb {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinvanaApplicationWeb.class);
+
+    }
+}
